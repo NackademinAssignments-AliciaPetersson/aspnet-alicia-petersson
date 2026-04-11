6ce5a2a1a1a86ad93fff533174f659efb8073471
Lizza96 <ia-hugo@hotmail.com>
2026-04-11 23:20:26 +0200

    Created MembershipController. Extracted aside profile picture with profile menu options into its own Partial view

---
+++ Presentation.WebApp/Areas/Account/Controllers/MembershipController.cs
@@ -0,0 +1,34 @@
+﻿using Application.Abstractions.Services;
+using Application.Modules.Members;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.WebApp.Areas.Account.Models;
+using System.Security.Claims;
+
+namespace Presentation.WebApp.Areas.Account.Controllers;
+
+[Area("Account")]
+[Route("account")]
+[Authorize(Roles = "Member")]
+public class MembershipController(IMemberService memberService) : Controller
+{
+    [HttpGet("my-membership")]
+    public async Task<IActionResult> MyMembership()
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (string.IsNullOrWhiteSpace(userId))
+            return RedirectToAction(nameof(SignOut));
+
+        var accountResult = await memberService.GetMemberDetailsAsync(userId);
+        if (!accountResult.Success)
+            return RedirectToAction(nameof(SignOut));
+
+        var viewModel = new MyMembershipViewModel
+        {
+            ProfileImageUrl = accountResult.Value?.ImageUrl ?? "/images/default_profile_image.png"
+        };
+
+
+        return View(viewModel);
+    }
+}
---
+++ Presentation.WebApp/Areas/Account/Models/MyMembershipViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Presentation.WebApp.Areas.Account.Models;
+
+public class MyMembershipViewModel
+{
+    public string? ProfileImageUrl { get; set; }
+}
