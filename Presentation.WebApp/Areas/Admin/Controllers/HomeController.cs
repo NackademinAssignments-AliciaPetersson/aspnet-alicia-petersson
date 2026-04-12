019397c12d12d66ada8ff910eb4b9c5825f49430
Lizza96 <ia-hugo@hotmail.com>
2026-04-13 00:11:11 +0200

    Added Admin Area

---
+++ Presentation.WebApp/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.WebApp.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = "Admin")]
+[Route("admin")]
+public class HomeController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return RedirectToAction(nameof(Dashboard));
+    }
+
+    [HttpGet("dashboard")]
+    public IActionResult Dashboard()
+    {
+        return View();
+    }
+}
