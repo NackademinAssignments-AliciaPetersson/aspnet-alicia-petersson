c77831d8cab6289b912bce7e84bcb6061169b725
Lizza96 <ia-hugo@hotmail.com>
2026-04-12 11:00:12 +0200

    Added MembershipTypeEntity ands it's configuration. Migrationn added

---
+++ Infrastructure/Persistence/EFC/Configurations/MembershipTypeConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Infrastructure.Persistence.EFC.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.EFC.Configurations;
+
+internal sealed class MembershipTypeConfiguration : IEntityTypeConfiguration<MembershipTypeEntity>
+{
+    public void Configure(EntityTypeBuilder<MembershipTypeEntity> builder)
+    {
+        builder.ToTable("MembershipTypes");
+
+        builder.HasKey(e => e.Id);
+
+        // Properties
+        builder.Property(e => e.Name).IsRequired().HasMaxLength(50);
+        builder.Property(e => e.BasePrice).IsRequired().HasColumnType("decimal(18,2)");
+        builder.Property(e => e.IsActive).IsRequired().HasDefaultValue(true);
+
+        // Indexes
+        builder.HasIndex(e => e.Name)
+            .IsUnique()
+            .HasDatabaseName("UQ_MembershipTypes_Name");
+
+        builder.HasIndex(e => e.IsActive)
+            .HasDatabaseName("IX_MembershipTypes_IsActive");
+
+        //Checks
+        builder.ToTable(tb => {
+            tb.HasCheckConstraint("CK_MembershipTypes_BasePriceNotNegative", "[BasePrice] >= 0");
+            tb.HasCheckConstraint("CK_MembershipTypes_NameNotEmpty", "LTRIM(RTRIM([Name])) <> ''");
+        });
+    }
+}
--- Infrastructure/Persistence/EFC/Migrations/20260412085735_MembershipTypeEntityAdded.Designer.cs
+++ Infrastructure/Persistence/EFC/Migrations/20260412085735_MembershipTypeEntityAdded.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Persistence.EFC.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Persistence.EFC.Migrations
 {
     [DbContext(typeof(CoreFitnessContext))]
-    partial class CoreFitnessContextModelSnapshot : ModelSnapshot
+    [Migration("20260412085735_MembershipTypeEntityAdded")]
+    partial class MembershipTypeEntityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
